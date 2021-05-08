ee41e19a155c0df3a04a79373c21951c50ad2427
Hanif Watson <jeffrey.h.watson@protonmail.com>
2021-05-07 22:07:41 -0700

    modeling write up

--- src/classes.py
+++ src/classes.py
@@ -23,15 +23,15 @@ class Harness:
     
     def __init__(self, scorer, random_state=2021):
         self.scorer = scorer
-        self.history = pd.DataFrame(columns=['Name', 'Accuracy', 'Notes'])
+        self.history = pd.DataFrame(columns=['Name', 'Accuracy (F1)', 'Notes'])
 
     def report(self, model, X, y, name, notes='', cv=5,):
         scores = cross_val_score(model, X, y, 
                                  scoring=self.scorer, cv=cv)
-        frame = pd.DataFrame([[name, scores.mean(), notes]], columns=['Name', 'Accuracy', 'Notes'])
+        frame = pd.DataFrame([[name, scores.mean(), notes]], columns=['Name', 'Accuracy (F1)', 'Notes'])
         self.history = self.history.append(frame)
         self.history = self.history.reset_index(drop=True)
-        self.history = self.history.sort_values('Accuracy')
+        self.history = self.history.sort_values('Accuracy (F1)')
         self.print_error(name, scores.mean())
 #         print(scores)
         return scores
